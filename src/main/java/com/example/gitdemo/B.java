53e19c10be6b2a2bf0316fdcb11b79d78bed8721
one <wrcj12138@163.com>
2021-10-23 16:55:27 +0800

    - 第三次提交：新增 B 类

---
+++ src/main/java/com/example/gitdemo/B.java
@@ -0,0 +1,12 @@
+package com.example.gitdemo;
+
+/**
+ * @author one
+ * @version 1.0.0
+ * @since 2021/10/23
+ */
+public class B {
+    public void method(String[] args) {
+        System.out.println(this.getClass());
+    }
+}
