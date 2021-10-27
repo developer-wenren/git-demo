a52ad73ec13a1ab1e1e4a8f59438ab6500897cf8
one <wrcj12138@163.com>
2021-10-27 22:05:48 +0800

    - 第七次提交：新增类 E - 补充 E 类方法 - print word

---
+++ src/main/java/com/example/gitdemo/E.java
@@ -0,0 +1,12 @@
+package com.example.gitdemo;
+
+/**
+ * @author one
+ * @version 1.0.0
+ * @since 2021/10/25
+ */
+public class E {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
